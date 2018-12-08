501a3c4d66f6bb5a2a12c5f3159ae0e6f39fef51
Sacha <sacha.auday@hotmail.fr>
2018-12-08 19:52:11 +0200

    priority task done + sounds added

--- logic.js
+++ logic.js
@@ -1,3 +1,12 @@
+var Sound = {
+  done : new Audio('sounds/done.mp3'),
+  priority : new Audio('sounds/priority.aac'),
+  trash : new Audio('sounds/trash.mp3'),
+  input : new Audio('sounds/input.mp3'),
+  undone : new Audio('sounds/undone.mp3'),
+  error : new Audio('sounds/error.mp3'),
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -5,7 +14,8 @@ class App extends React.Component {
       activities: [],
       activitiesDone: [],
       key: 500,
-      key2: 1000
+      key2: 1000,
+      placeholder: "Write your task here"
     };
     this.addTask = this.addTask.bind(this)
     this.moveTask = this.moveTask.bind(this)
@@ -13,28 +23,38 @@ class App extends React.Component {
     this.unDoneTask = this.unDoneTask.bind(this)
     this.addPriority = this.addPriority.bind(this)
   }
+
   addTask(event) {
-    if (event.target.tagName === "BUTTON" || event.keyCode === 13){
-    this.state.activities.push(
-      <li id="tasks" key={this.state.key + 1}>
-        {this.textInput.value} on{" "}
-        <u>
-          {this.day.value}/{this.month.value}/{this.year.value}
-        </u>
-        <ButtonList bgColor="red" img="delete" onClick={this.removeTask} />
-        <ButtonList bgColor="green" img="star" onClick={this.addPriority}/>
-        <ButtonList bgColor="blue" img="checked" onClick={this.moveTask} />
-      </li>
-    )
+    if (this.textInput.value.length === 0){
+      var newPlaceholder = "Please enter at least one character"
+      this.setState({
+        placeholder : newPlaceholder
+      })
+    }
+    else if (event.target.tagName === "BUTTON" || event.keyCode === 13){
+      Sound.input.play();
+      this.state.activities.push(
+        <li id="tasks" key={this.state.key + 1}>
+         {this.textInput.value} on{" "}
+         <u>
+           {this.day.value}/{this.month.value}/{this.year.value}
+         </u>
+         <ButtonList bgColor="red" img="delete" onClick={this.removeTask} />
+         <ButtonList bgColor="green" img="star" onClick={this.addPriority}/>
+         <ButtonList bgColor="blue" img="checked" onClick={this.moveTask} />
+        </li>
+      )
     this.setState({
       activities: this.state.activities,
-      key: this.state.key + 1
+      key: this.state.key + 1,
+      placeholder : "Write your task here"
     });
     this.textInput.value = "";
     }
   }
 
   moveTask(event) {
+    Sound.done.play();
     var taskSelected = event.target.parentNode.parentNode.parentNode.parentNode;
     this.state.activitiesDone.push(taskSelected.textContent);
     this.setState({
@@ -45,16 +65,25 @@ class App extends React.Component {
 
   addPriority(event){
     var taskSelected = event.target.parentNode.parentNode.parentNode.parentNode;
-    console.log(taskSelected)
-    taskSelected.style.backgroundColor = "white" ? "yellow" : "white"
+    var listContainer = taskSelected.parentNode
+    if(taskSelected.id === "tasks") {
+      Sound.priority.play();
+      taskSelected.setAttribute("id","tasksPriority")
+      listContainer.prepend(taskSelected)
+    } else{
+      taskSelected.setAttribute("id","tasks")
+      listContainer.append(taskSelected)
+    }
   }
 
   removeTask(event) {
+    Sound.trash.play();
     var taskSelected = event.target.parentNode.parentNode.parentNode.parentNode;
     taskSelected.remove();
   }
 
   unDoneTask(event) {
+    Sound.undone.play();
     var taskSelected = event.target.parentNode.parentNode.parentNode.parentNode;
     this.state.activities.push(
       <li id="tasks" key={this.state.key2 + 1}>
@@ -100,13 +129,10 @@ class App extends React.Component {
     return (
       <div>
         <Title />
-        <input id="inputTask" type="text" placeholder="Write your task here" onKeyUp={this.addTask}
-          ref={input => {
-            this.textInput = input;
-          }}
-        />
-        <div id="selectDate">Select the date :</div>
+        <input id="inputTask" type="text" placeholder={this.state.placeholder} onKeyUp={this.addTask}
+          ref={input => {this.textInput = input;}}/>
 
+        <div id="selectDate">Select the date :</div>
         <select ref={input => {this.day = input;}}>
           {this.renderOptions(dateUtils.days)}
         </select>
@@ -190,6 +216,7 @@ class Trash extends React.Component {
     this.openTrash = this.openTrash.bind(this);
     this.closeTrash = this.closeTrash.bind(this);
   }
+
   openTrash() {
     this.setState({
       display: "block"
